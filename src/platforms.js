4246f1dba40f71cc4bd348c1d5b3fb7da01471df
Sebastian Seitz <bassistsagainstracists@gmx.net>
2022-05-26 20:14:36 +0200

    add boulders

--- src/platforms.js
+++ src/platforms.js
@@ -7,39 +7,51 @@ export function groundLevel () {
   return Math.max(...k.get('wall').map(wall => wall.pos.y))
 }
 
+function tilePos (pos, delta) {
+  return pos.add(delta.scale(TILE_SIZE))
+}
+
 function addGround (pos, x, y) {
   if (k.chance(0.33)) {
-    spawnPlant(pos.add(k.UP.scale(TILE_SIZE)))
+    spawnPlant(tilePos(pos, k.UP))
   }
 
-  return spawnWall(pos, x, y).pos.add(k.RIGHT.scale(TILE_SIZE))
+  return tilePos(spawnWall(pos, x, y).pos, k.RIGHT)
 }
 
 function addGem (start, length) {
-  return spawnGem(start.add(k.vec2(
-    k.randi(length),
-    k.randi(-3)
-  ).scale(TILE_SIZE)))
+  const delta = k.vec2(k.randi(length), k.randi(-3))
+  return spawnGem(tilePos(start, delta))
+}
+
+function addBoulder (pos) {
+  const delta = k.vec2(0, k.randi(-1, -4))
+  return spawnWall(tilePos(pos, delta), 1, 1)
 }
 
 /**
  * @param {import('kaboom').Vec2} start
  * @param {number} length
+ * @param {number} count
  * @returns {import('kaboom').Vec2}
  */
-function addPlatform (start, length) {
+function addPlatform (start, length, count) {
+  const gemPosX = count === 0 ? -1 : addGem(start, length).pos.x
+
   const pos = range(length - 2).reduce(pos => {
+    if (pos.x !== gemPosX && k.chance(count / 100)) {
+      addBoulder(pos)
+    }
+
     return addGround(pos, 1, 0)
   }, addGround(start, 0, 0))
 
-  return addGround(pos, 2, 0).add(
-    k.randi(2, 3) * TILE_SIZE,
-    k.randi(-2, 2) * TILE_SIZE
-  )
+  const delta = k.vec2(k.randi(2, 3), k.randi(-2, 2))
+  return tilePos(addGround(pos, 2, 0), delta)
 }
 
 export function platformGenerator (pos, maxLength) {
-  let isFirst = true
+  let count = 0
 
   return function next () {
     if (k.toScreen(pos).x > k.width() * 2) {
@@ -47,13 +59,8 @@ export function platformGenerator (pos, maxLength) {
     }
 
     const length = k.randi(maxLength)
+    pos = addPlatform(pos, length, count++)
 
-    if (!isFirst) {
-      addGem(pos, length)
-    }
-
-    pos = addPlatform(pos, length)
-    isFirst = false
     next()
   }
 }
